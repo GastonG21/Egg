400a1fb31aebc5b61abd051e1f66442de46b77be
Gaston Garcia <gaston_garcia_2015@hotmail.com>
2022-09-22 17:50:41 -0300

    Primer commit

---
+++ src/ej1guia1/Ej1Guia1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej1guia1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GastonG
+ */
+public class Ej1Guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        int num;
+        int num2;
+        int resultado;
+        
+        System.out.println("Ingrese su primer numero");
+        num = leer.nextInt();
+        
+        System.out.println("Ingrese su segundo numero");
+        num2= leer.nextInt();
+        
+        resultado = num + num2;
+        
+        System.out.println("El resultado de los numeros ingresados es: " + resultado);
+        
+        
+        
+        
+    }
+    
+}
